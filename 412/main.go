fa059429a2997ee2d7f6310e467766d042a390d8
Pichugin Dmitry <pichugin.dmitry.v@gmail.com>
2019-04-16 21:33:05 +0300

    добавил решение 412 задачи

---
+++ 412/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(fizzBuzz(30))
+}
+
+func fizzBuzz(n int) []string {
+
+	result := make([]string, 0, n)
+
+	for i := 1; i <= n; i++ {
+
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			result = append(result, "FizzBuzz")
+		case i%3 == 0:
+			result = append(result, "Fizz")
+		case i%5 == 0:
+			result = append(result, "Buzz")
+		default:
+			result = append(result, strconv.Itoa(i))
+
+		}
+
+	}
+
+	return result
+}
